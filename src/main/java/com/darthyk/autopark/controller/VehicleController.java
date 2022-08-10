0a6a9aa3eb4b2861e58bf25702dd3d10805e4e21
darthyk <vsazhin@wiley,com>
2022-08-10 21:37:46 +0300

    Add freemarker ui

--- src/main/java/com/darthyk/autopark/controller/VehicleController.java
+++ src/main/java/com/darthyk/autopark/controller/VehicleController.java
@@ -1,13 +1,9 @@
 package com.darthyk.autopark.controller;
 
-import com.darthyk.autopark.dto.VehicleDto;
 import com.darthyk.autopark.service.VehicleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,19 +12,8 @@ public class VehicleController {
     @Autowired
     private VehicleService vehicleService;
 
-    @PostMapping
-    public String createVehicle(@RequestBody VehicleDto dto) {
-        vehicleService.createVehicle(dto);
-        return "OK";
-    }
-
-    @GetMapping("/{id}")
-    public VehicleDto getVehicle(@PathVariable(name = "id") Long id) {
-        return vehicleService.getVehicle(id);
-    }
-
-    @GetMapping("/vehicles")
-    public String index(Model model) {
+    @GetMapping({"/","/vehicles"})
+    public String vehicles(Model model) {
         model.addAttribute("vehicleList", vehicleService.getAllVehicles());
         return "index";
     }
